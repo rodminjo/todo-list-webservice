eabcb4f7b25ba9817bb70eb14e13d451d5ea62c6
rodminjo <rodminjo@naver.com>
2023-04-04 23:22:23 +0900

    230404 Posts update

--- src/main/java/todo/list_service/domain/posts/Posts.java
+++ src/main/java/todo/list_service/domain/posts/Posts.java
@@ -3,19 +3,32 @@ package todo.list_service.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import todo.list_service.domain.BaseTimeEntity;
+import todo.list_service.domain.replyposts.ReplyPosts;
+import todo.list_service.domain.uploadPosts.UploadPosts;
+import todo.list_service.domain.user.User;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Getter
 @NoArgsConstructor
+@DynamicInsert
 @Entity
 public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "POSTS_ID")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
 
     @Column(length = 500, nullable = false)
     private String title;
@@ -23,24 +36,32 @@ public class Posts extends BaseTimeEntity {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
-    @Column(columnDefinition = "boolean default false")
-    private Boolean checked;
+    @Column
+    @ColumnDefault("0")
+    private Integer viewCount;
 
-    private String author;
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.ALL)
+    private List<UploadPosts> uploadPosts= new ArrayList<>();
+
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.ALL)
+    private List<ReplyPosts> replyPosts= new ArrayList<>();
+
 
     @Builder
-    public Posts(String title, String content, Boolean checked, String author) {
+    public Posts(User user, String title, String content, Integer viewCount) {
         this.title = title;
         this.content = content;
-        this.checked = checked;
-        this.author = author;
+        this.user = user;
+        this.viewCount = viewCount;
     }
 
-    public void update(String title, String content, Boolean checked) {
+    public void update(String title, String content) {
         this.title = title;
         this.content = content;
-        this.checked = checked;
+    }
 
+    public void updateViewCount(){
+        this.viewCount++;
     }
 
 }
